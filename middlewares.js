1e6d928f43c459f6274675d03797bc16243a27fd
gyuhwanhwang <rbghks0327@gmail.com>
2020-01-10 00:21:06 +0900

    #2.6 Upload pug

--- middlewares.js
+++ middlewares.js
@@ -3,7 +3,7 @@ export const localMidlleware = (req, res, next) => {
     res.locals.siteName = "Oup";
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated: false,
+        isAuthenticated: true,
         id: 1
     };
     next();
